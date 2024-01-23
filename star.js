bf6b3cc5ea054d3deb244162773b93aecbce99b3
chrissDev <dushimec515@gmail.com>
2024-01-23 15:05:37 -0800

    calclator

---
+++ star.js
@@ -0,0 +1,10 @@
+function triangel () {
+    for (let i = 0; i <=3; i++) {
+        for (let j = 0; j <=10; j++) {
+           process.stdout.write("*")
+        }
+        console.log("");
+    }
+}
+
+triangel()
